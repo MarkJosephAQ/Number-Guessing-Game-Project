09f93333994b45f56379bd89ffbd447133d3f353
Mark Joseph Almazan Aquino <markjosephaquino_a@yahoo.com>
2024-08-25 22:31:51 +0800

    added welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        String welcomeMessage = """
+                Welcome to the Number Guessing Game!\s
+                I'm thinking of a number between 1 - 100\s
+                You have 3 tries to guess before Game Over\s
+                Good luck!""";
+
+        System.out.println(welcomeMessage);
+
+    }
 }
